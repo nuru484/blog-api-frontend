07ab9964d04e804043719f3950daade6a00772ce
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-10-14 22:07:22 +0000

    wrap authcontextprovider to app and add setaccess and refresh acess in loginform

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,11 @@
 // src/components/LoginForm.jsx
 import React, { useState } from 'react';
 import loginFetch from '../api/login';
+import useAuth from '../hooks/useAuth';
 
 const LoginForm = () => {
+  const { setAccessToken, setRefreshToken } = useAuth();
+
   const [credentials, setCredentials] = useState({ email: '', password: '' });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -13,7 +16,18 @@ const LoginForm = () => {
     setError(null);
     try {
       const response = await loginFetch(credentials);
+
+      if (!response.ok) {
+        const data = await response.json();
+        const errorMessage = data.error.message;
+        console.log(errorMessage);
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+
       console.log('Logged in successfully');
+      const data = await response.json();
+      setAccessToken(data.accessToken);
+      setRefreshToken(data.refreshToken);
     } catch (error) {
       console.error(`An error occurred: ${error.message}`);
       setError(`An error occurred: ${error.message}`);
